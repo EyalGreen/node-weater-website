8eab52a88de95466865acc03e63b1e66d49ee9eb
your username <eyal.green4@gmail.comgit config --global user.name eyalgreenuser.email eyal.green4@gmail.com>
2021-01-10 17:10:40 +0200

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = '';
 
     var address = search.value;
-    fetch('http://localhost:3000/weather?address=' + address).then((response) => {
+    fetch('/weather?address=' + address).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
